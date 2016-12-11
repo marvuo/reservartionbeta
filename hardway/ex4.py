39f24d57d9d6bb5805ba4d21945c6b2f268670b8
Markus Vuorinen <markus.vuorinen@gmail.com>
2016-12-11 19:20:34 +0000

    hardware try2

---
+++ hardway/ex4.py
@@ -0,0 +1,6 @@
+print ("Just text")
+print "ilman sulkuja", 25.0 / 4.0 + 4
+print "modulus ", 25 % 4
+car = 100
+seating = 4
+print "there are ", car," cars which each seat ",seating, "in total of ", car*seating, "seats available" 
---
+++ hardway/ex5.py
@@ -0,0 +1,6 @@
+weight = 80
+height = 180
+name = "Markus"
+print "name", name
+
+print "My name is i weight %d, with height of %d, and total bmi of %d."  % (weight, height, (weight / ((height/100) * (height/100))))
